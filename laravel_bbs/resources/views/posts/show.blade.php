f638d10743eaa6f9eaed76fc20aa2f82570df692
柴田将馬 <>
2019-04-16 10:28:51 +0900

    ボタン位置の修正

--- laravel_bbs/resources/views/posts/show.blade.php
+++ laravel_bbs/resources/views/posts/show.blade.php
@@ -3,23 +3,23 @@
 @section('content')
     <div class="container mt-4">
         <div class="border p-4">
+            <div style="text-align: right">
+                <div style="display: inline-block;" class="mb-4 text-right">
+                    <a class="btn btn-primary" href="{{ route('posts.edit', ['post' => $post]) }}">
+                        編集する
+                    </a>
+                </div>
+                <form
+                    style="display: inline-block;"
+                    method="POST"
+                    action="{{ route('posts.destroy', ['post' => $post]) }}"
+                >
+                    @csrf
+                    @method('DELETE')
 
-            <div style="display: inline-block;" class="mb-4 text-right">
-                <a class="btn btn-primary" href="{{ route('posts.edit', ['post' => $post]) }}">
-                    編集する
-                </a>
+                    <button class="btn btn-danger">削除する</button>
+                </form>
             </div>
-            <form
-                style="display: inline-block;"
-                method="POST"
-                action="{{ route('posts.destroy', ['post' => $post]) }}"
-            >
-                @csrf
-                @method('DELETE')
-
-                <button class="btn btn-danger">削除する</button>
-            </form>
-
             <h1 class="h5 mb-4">
                 {{ $post->title }}
             </h1>
